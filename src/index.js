682b6b627a7b613759185aa560c4f6cf25a43d31
Roman Zhuk <ferdigo999@gmail.com>
2020-10-17 22:35:28 +0300

    feat: implemented code works with first parameter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  
+  let open =[];
+  let closed = []
+let squareOpenBracket = '['
+let squareClosedBracket = ']'
+let openBracket = '('
+let closedBracket = ')'
+let curlyOpenBracket = '{'
+let curlyClosedBracket = '}'
+
+if(str.length % 2 != 0 || typeof(str) === 'number') return false
+
+console.log(str)
+for(let element of str){
+    if(element == squareOpenBracket || element == openBracket || element == curlyOpenBracket){
+        open.push(element)
+    }
+    if(element == squareClosedBracket || element == closedBracket || element == curlyClosedBracket){
+        closed.push(element)
+    }
+}
+
+if (open.length == closed.length) return true
+return false
+
+
 }
