2c20dbd26ecc94c0232f1b6969d2f8efebe3f8d4
Roman Zhuk <ferdigo999@gmail.com>
2020-10-25 22:56:37 +0200

    feat: function completed

--- src/index.js
+++ src/index.js
@@ -3,32 +3,24 @@ module.exports = function check(str, bracketsConfig) {
     let closed = [];
     let holder = [];
 
-    bracketsConfig.forEach(pair => {
+    bracketsConfig.forEach((pair) => {
         opened.push(pair[0]);
-        closed.push(pair[1])
+        closed.push(pair[1]);
     });
 
-    console.log('opened = ', opened)
-    console.log('closed = ', closed)
-  
     for (let char of str) {
-          
-         if (opened.includes(char)) {
+        if (holder.includes(char) && closed.includes(char)) {
+            holder.pop();
+        } else if (opened.includes(char)) {
             holder.push(char);
-          
+        } else if (closed.includes(char)) {
+            const ind = opened[closed.indexOf(char)];
+            if (holder[holder.length - 1] === ind) {
+                holder.pop();
+            } else {
+                holder.push(char);
+            }
         }
-        if (closed.includes(char)) {
-          // console.log(char)
-            if (holder.length != 0) {
-              let ind = closed.findIndex(item => item === char)
-              console.log(ind)
-              console.log(holder[ind])
-                holder.pop(holder[ind])
-            } else return false
-
-        }
-
     }
-console.log(holder)
-    return true;
+    return !holder.length;
 };
